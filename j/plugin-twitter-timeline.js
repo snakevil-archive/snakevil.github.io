35cf1115646d1fe0b5ed33da45030fc50d2e4065
Snakevil Zen <zsnakevil@gmail.com>
2013-04-23 00:26:05 +0800

    Beautified `Twitter Timeline' plugin.

--- j/plugin-twitter-timeline.js
+++ j/plugin-twitter-timeline.js
@@ -1,33 +1,86 @@
-(function(d,c,f) {
+(function(c, r, f) {
   if (!XMLHttpRequest) return;
-  var a = d.getElementsByClassName(c);
+  var a = document.getElementsByClassName(c);
   for (var i = a.length - 1; i >= 0; i--) {
-    if (!a[i].id) continue;
+    if (!a[i].id) {
+      continue;
+    }
     var j = new XMLHttpRequest();
+    j.format = f;
     j.box = a[i];
-    j.onload = f;
+    j.onload = r;
     j.open('get', 'https://szen.in/twtl?' + a[i].id, true);
     j.send();
   }
-})(document, 'plugin-twitter-timeline', function() {
+})('plugin-twitter-timeline', function() {
   eval('var j=' + this.responseText);
-  if ('e89a5e7afae392e0a76a5f98ffdfa9e7f72aa6e6' != j.token) return;
+  if ('e89a5e7afae392e0a76a5f98ffdfa9e7f72aa6e6' != j.token ||
+    !j.records.length
+  ) {
+    return;
+  }
+  document.getElementsByClassName('page-sidebar')[0].style.display = 'block';
+  this.box.style.display = 'block';
   var l = 10;
   if (this.box.hasAttribute('size')) {
     l = Math.floor(this.box.getAttribute('size'));
-    if (isNaN(l)) l = 10;
+    if (isNaN(l)) {
+      l = 10;
+    }
   }
-  var h = '<ul>';
+  var h = '<legend><a href="' + j.root + '">Twitter Timeline</a></legend>' +
+    '<ul>';
   for (var i = 0; i < l; i++) {
-    h += '<li style="background-image:url(\"' + j.records[i].avatar + '\")">' +
-      '<a class="speaker' + (j.records[i].retweeted ? ' retweet' : '') +
-      '" href="' + j.records[i].ref + '">' + j.records[i].speaker + '</a>' +
+    h += '<li' + (j.records[i].retweeted ? ' class="retweet"' : '') + '>' +
+      '<a class="speaker" href="' + j.records[i].ref + '">' +
+      '<img src="' + j.records[i].avatar + '" width="16" />' +
+      j.records[i].speaker + '</a>' +
       '<span class="words">' + j.records[i].words + '</span>' +
       '<a class="time" href="' + j.records[i].ref + '">' +
-      j.records[i].time + '</a>' +
+      this.format(j.records[i].time) + '</a>' +
       '</li>';
-    console.log(j.records[i]);
   }
   h += '</ul>';
   this.box.innerHTML = h;
+}, function(t) {
+  var d = new Date(t);
+  var h = '<time datetime="' + d.getUTCFullYear() + '-' +
+    ((8 < d.getUTCMonth()) ?
+      (1 + d.getUTCMonth()) :
+      ('0' + (1 + d.getUTCMonth()))
+    ) + '-' +
+    ((9 < d.getUTCDate()) ? d.getUTCDate() : ('0' + d.getUTCDate())) + 'T' +
+    ((9 < d.getUTCHours()) ? d.getUTCHours() : ('0' + d.getUTCHours())) + ':' +
+    ((9 < d.getUTCMinutes()) ?
+      d.getUTCMinutes() :
+      ('0' + d.getUTCMinutes())
+    ) + ':' +
+    ((9 < d.getUTCSeconds()) ?
+      d.getUTCSeconds() :
+      ('0' + d.getUTCSeconds())
+    ) + 'Z">';
+  d = Math.round(Date.now() / 1000) - t;
+  console.log(Date.now() + ',' + t);
+  if (60 > d) {
+    h += 'now';
+  } else {
+    d = Math.round(d / 60);
+    if (60 > d) {
+      h += d + ' minutes ago';
+    } else {
+      d = Math.round(d / 60);
+      if (24 > d) {
+        h += d + ' hours ago';
+      } else {
+        d = Math.round(d / 24);
+        if (7 > d) {
+          h += d + ' days ago';
+        } else {
+          h += Math.round(d / 7) + ' weeks ago';
+        }
+      }
+    }
+  }
+  h += '</time>';
+  return h;
 });
